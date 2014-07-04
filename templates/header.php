2b95635d87f45164bdb8b0e8ee484d2bd4df043b
Robin Siep <mail@robin-siep.nl>
2014-07-04 17:05:52 +0200

    initial commit

---
+++ templates/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="../css/stylesheet.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="../css/responsiveslides.css">
+	<script src="../js/jquery-1.9.1.min.js"></script>
+	<script src="../js/responsiveslides.js"></script>
+	<link href="//example.com/path/to/video-js.css" rel="stylesheet">
+	
+	<!-- bootstrap -->
+	<script src="../js/bootstrap.min.js"></script>
+	<link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	
+	<script>
+	  videojs.options.flash.swf = "http://example.com/path/to/video-js.swf"
+	</script>
+	
+</head>
+<body>
+	<!-- Nav -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a class="navbar-brand" href="#page-top">1 2 QUIT</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#trailer">Trailer</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#about">How it works</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
